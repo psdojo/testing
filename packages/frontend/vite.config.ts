9099e88182412b221327e6fdecac72ea0cf7dfd3
Pranjal Sharma <pranjalsharmaw@gmail.com>
2025-03-05 18:55:44 -0500

    fixed

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  host: 0.0.0.0,
+  host: "0.0.0.0",
   port: process.env.PORT || 5173,
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
