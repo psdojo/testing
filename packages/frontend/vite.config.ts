7d5a0b2bcf19092a95aa0d3e4d570e3e28e7e534
Pranjal Sharma <pranjalsharmaw@gmail.com>
2025-03-05 18:52:15 -0500

    updated

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,13 +3,8 @@ import react from '@vitejs/plugin-react'
 import path from "path"
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-<<<<<<< HEAD
-  host: '0.0.0.0',
-  port: 5173,
-=======
   host: 0.0.0.0,
   port: process.env.PORT || 5173,
->>>>>>> 11f3292 (updated the ports for the packages)
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
