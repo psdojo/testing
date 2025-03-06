94140ef5979494b1998ce642560bdf4029cc6b80
Pranjal Sharma <pranjalsharmaw@gmail.com>
2025-03-05 22:02:45 -0500

    update

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   function handleSubmit(e) {
     e.preventDefault()
     const url = e.target.inputUrl.value
-    axios.post('localhost:port', { url }).then((response) => { console.log(response) }).catch((error) => { console.log(error) })
+    axios.post('https://testing-uneh.onrender.com/', { url }).then((response) => { console.log(response) }).catch((error) => { console.log(error) })
     console.log(url)
   }
 
